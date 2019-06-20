f72f6b252b10fb5ea0b1b06bee2b955069a04ffc
Tais-WD <taisiia.efremova@gmail.com>
2019-06-20 15:20:58 +0200

    second commit

---
+++ resources/js/components/About.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    
+    render () {
+        return (          
+            <div id="about-wrap">
+                <div className="container">
+
+                    <div className="big-icon">
+                        <img src="img/icon/search3.svg" alt="garbage" className="big"/>
+                    </div>
+
+                    <h2>- About Bin Icon -</h2>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/1-plastic1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/2-namagomi2.svg" alt="namagomi" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                            <img src="img/icon/3-glass2.svg" alt="glass" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                            <img src="img/icon/4-glass1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                            <img src="img/icon/5-paper1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/6-battery1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/7-can1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/8-textil1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-xs-3">
+                        <img src="img/icon/9-cloth1.svg" alt="plastic" className="explainIcon"/>
+                        </div>
+                        <div className="col-xs-9">
+                        EN:Plastic Bag<br/>
+                        CZ:Plastic Bag
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+        
+    }
+}
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,16 +1,80 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Heading from './Heading';
-import MapContainer from './MapContainer';
-import Menu from './Menu';
 
+import MapContainer from './MapContainer';
+
+import About from './About';
+import Contact from './Contact';
+import Login from './Login';
 export default class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            page: 'map'
+        }
+    }
+
     render() {
         return (
             <div>
-                <Heading/>
-                <MapContainer/>
-                <Menu />
+                {
+                    (()=>{
+                        if(this.state.page == 'map')
+                            return (<MapContainer/>);
+                        else if(this.state.page == 'about')
+                            return (<About/>)
+                            else if(this.state.page == 'contact')
+                            return (<Contact/>)
+                            else if(this.state.page == 'login')
+                            return (<Login/>)
+                    })()
+                }
+  
+                <nav className="menu" id="theMenu">
+                    <div className="row">
+                        <div className="col-3 menu-bar">
+                        <a href="#" onClick={()=>{
+                            this.setState({
+                                page: 'map'
+                            })
+                        }}>
+                            <img src="img/icon/map1.svg" alt="map" className="menu-image"/><br/>
+                            Map
+                        </a>
+                        </div>
+                        <div className="col-3 menu-bar ">
+                        <a href="#" onClick={()=>{
+                            this.setState({
+                                page: 'about'
+                            })
+                        }}>
+                            <img src="img/icon/waste2.svg" alt="waste" className="menu-image"/><br/>
+                            About
+                        </a>
+                        </div>
+                        <div className="col-3 menu-bar">
+                        <a href="#" onClick={()=>{
+                            this.setState({
+                                page: 'contact'
+                            })
+                        }}>
+                            <img src="img/icon/contact1.svg" alt="contact" className="menu-image"/><br/>
+                            Contact
+                        </a>
+                        </div>
+                        <div className="col-3 menu-bar">
+                        <a href="#" onClick={()=>{
+                            this.setState({
+                                page: 'login'
+                            })
+                        }}>
+                            <img src="img/icon/login.svg" alt="login"  className="menu-image"/><br/>
+                            Login
+                        </a>
+                        </div>
+                    </div>
+                </nav>
             </div>
         );
     }
