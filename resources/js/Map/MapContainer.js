e924936cb1b24cf944ed3276028592136674da4f
Tais-WD <taisiia.efremova@gmail.com>
2019-07-16 10:58:16 +0200

    some

--- resources/js/Map/MapContainer.js
+++ resources/js/Map/MapContainer.js
@@ -205,14 +205,10 @@ class MapContainer extends Component {
             <Marker
                 icon={myPositionicon}
                 name={"SOMA"}
-<<<<<<< HEAD
                 position={{
-                    lat: this.state.lat,
-                    lng: this.state.lng
+                    lat: this.state.userPosition.lat,
+                    lng: this.state.userPosition.lng
                 }}
-=======
-                position={{ lat: this.state.userPosition.lat, lng: this.state.userPosition.lng }}
->>>>>>> 9f19d1f46c4c8bdc9d135de4c1c45940187dcc7b
 
                 //this.markerClicked.bind(this)
             />
