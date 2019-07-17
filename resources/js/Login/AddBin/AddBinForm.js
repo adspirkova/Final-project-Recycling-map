49655005ac875c89575cec80ceaf161a0a676a62
Adela Spirkova <ad.spirkova@gmail.com>
2019-07-17 20:49:15 +0200

    inline styles

--- resources/js/Login/AddBin/AddBinForm.js
+++ resources/js/Login/AddBin/AddBinForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../../Contact/Contact.js';
+var csurf = require('csurf');
 
 
 // ******* For the images Start ************
@@ -120,7 +121,7 @@ export default class AddBinForm extends Component {
                 </div>
                     <br/>
                     <form className="contact-form php-mail-form" role="form" action="/addbin/create" method="POST">
-                    <input type="hidden" name="_token" value={csrf_token} />
+                    <input type="hidden" name="_csrf" value="{{csrfToken}}">
                         <div className="form-group">
                         <img src="img/icon/map2.svg" alt="location" className="contact-icon"/>
                             <label htmlFor="contact-name">Location</label>
